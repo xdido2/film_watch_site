32469f0382638c90620ea0ebf3fa41e12c9f16ea
AbdurashiDIDO <abdurashiddido@gmail.com>
2023-06-15 21:08:01 +0500

    fixed bugs

--- root/task.py
+++ root/task.py
@@ -5,7 +5,6 @@ from apps.movies.models.movie import Movie
 
 @shared_task
 def movie_data_form_api():
-    global movie_data
     r_data = requests.get(
         'https://videocdn.tv/api/movies?api_token=V5O2q1IlYYhqa3QFS5WOiKjj3xRLcihr&ordering=id&direction=des&page=1&limit=1').json()
     last_page = r_data.get('last_page', 0)
@@ -15,20 +14,43 @@ def movie_data_form_api():
             f'https://videocdn.tv/api/movies?api_token=V5O2q1IlYYhqa3QFS5WOiKjj3xRLcihr&ordering=id&direction=des&page={page}&limit=100').json()
         movie_data = data.get('data', [])
 
-    for content in movie_data:
-        objects = {
-            'ru_title': content.get('ru_title', ''),
-            'orig_title': content.get('orig_title', ''),
-            'released': content.get('released', ''),
-            'iframe_src': 'https:' + content.get('iframe_src', '')
-        }
+        for content in movie_data:
+            objects = {
+                'ru_title': content.get('ru_title', ''),
+                'orig_title': content.get('orig_title', ''),
+                'released': content.get('released', ''),
+                'iframe_src': 'https:' + content.get('iframe_src', '')
+            }
+import requests
+from celery import shared_task
+from apps.movies.models.movie import Movie
 
-        if not Movie.objects.filter(orig_title=objects['orig_title'],
-                                    ru_title=objects['ru_title']).exists() and objects is not None:
-            Movie.objects.create(**objects)
 
+@shared_task
+def movie_data_form_api():
+    r_data = requests.get(
+        'https://videocdn.tv/api/movies?api_token=V5O2q1IlYYhqa3QFS5WOiKjj3xRLcihr&ordering=id&direction=des&page=1&limit=1').json()
+    last_page = r_data.get('last_page', 0)
+
+    for page in range(1, last_page + 1):
+        data = requests.get(
+            f'https://videocdn.tv/api/movies?api_token=V5O2q1IlYYhqa3QFS5WOiKjj3xRLcihr&ordering=id&direction=des&page={page}&limit=100').json()
+        movie_data = data.get('data', [])
+
+        for content in movie_data:
+            objects = {
+                'ru_title': content.get('ru_title', ''),
+                'orig_title': content.get('orig_title', ''),
+                'released': content.get('released', ''),
+                'iframe_src': 'https:' + content.get('iframe_src', '')
+            }
+
+            if not Movie.objects.filter(orig_title=objects['orig_title'],
+                                        ru_title=objects['ru_title']).exists() and objects is not None:
+                Movie.objects.create(**objects)
+#
 # import requests
-
+#
 # from celery import shared_task
 #
 # from apps.movies.models.movie import Movie
@@ -42,9 +64,39 @@ def movie_data_form_api():
 #         data = requests.get(
 #             f'https://videocdn.tv/api/movies?api_token=V5O2q1IlYYhqa3QFS5WOiKjj3xRLcihr&ordering=id&direction=des&page={page}&limit=100').json()
 #         for i in range(len(data)):
-#             contant = data['data'][i]
-#             objects = {'ru_title': contant['ru_title'], 'orig_title': contant['orig_title'],
-#                        'released': contant['released'],
-#                        'iframe_src': 'https:' + contant['iframe_src']}
-#             if not Movie.objects.filter(orig_title=contant['orig_title'], ru_title=contant['ru_title']).exists():
+#             content = data['data'][i]
+#             objects = {'ru_title': content['ru_title'],
+#                        'orig_title': content['orig_title'],
+#                        'released': content['released'],
+#                        'iframe_src': 'https:' + content['iframe_src']}
+#             if not Movie.objects.filter(orig_title=content['orig_title'],
+#                                         ru_title=content['ru_title']).exists() and objects is not None:
+#                 Movie.objects.create(**objects)
+
+            if not Movie.objects.filter(orig_title=objects['orig_title'],
+                                        ru_title=objects['ru_title']).exists() and objects is not None:
+                Movie.objects.create(**objects)
+#
+# import requests
+#
+# from celery import shared_task
+#
+# from apps.movies.models.movie import Movie
+#
+#
+# @shared_task
+# def movie_data_form_api():
+#     r_data = requests.get(
+#         'https://videocdn.tv/api/movies?api_token=V5O2q1IlYYhqa3QFS5WOiKjj3xRLcihr&ordering=id&direction=des&page=1&limit=1').json()
+#     for page in range(1, r_data['last_page']):
+#         data = requests.get(
+#             f'https://videocdn.tv/api/movies?api_token=V5O2q1IlYYhqa3QFS5WOiKjj3xRLcihr&ordering=id&direction=des&page={page}&limit=100').json()
+#         for i in range(len(data)):
+#             content = data['data'][i]
+#             objects = {'ru_title': content['ru_title'],
+#                        'orig_title': content['orig_title'],
+#                        'released': content['released'],
+#                        'iframe_src': 'https:' + content['iframe_src']}
+#             if not Movie.objects.filter(orig_title=content['orig_title'],
+#                                         ru_title=content['ru_title']).exists() and objects is not None:
 #                 Movie.objects.create(**objects)
