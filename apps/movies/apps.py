9634d6717384d07f11fdd5bdce28ef8e3056f002
AbdurashiDIDO <abdurashiddido@gmail.com>
2023-07-20 14:14:22 +0500

    solve some bugs and add forgot password

--- apps/movies/apps.py
+++ apps/movies/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 # from apps.shared.tasks.get_genres_task import get_genres_from_api
 # from apps.shared.tasks.get_movies_task import get_movies_from_api
 
+
 class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.movies'
