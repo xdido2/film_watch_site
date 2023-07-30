a08688ca774afb8f7d699b82624892fb9bd6279b
Abdurashid <abdurashiddido@gmail.com>
2023-07-30 18:56:19 +0500

    add admin panel

--- apps/movies/models/movie_ordering.py
+++ apps/movies/models/movie_ordering.py
@@ -3,7 +3,7 @@ from django.db.models import Model, EmailField, URLField, TextField, CharField
 
 class Order(Model):
     email = EmailField()
-    content_title = CharField()
+    content_title = CharField(max_length=255)
     content_url = URLField()
     content_description = TextField()
 
